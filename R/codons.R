2aa095a5a34ff3c66b3d80d4e7c31706fc37f1d6
s243633 <s243633@dtu.dk>
2024-10-29 10:53:08 +0100

    s243633 code finish

---
+++ R/codons.R
@@ -0,0 +1,17 @@
+
+#' Creating codons
+#'
+#' @param sequence Clarifies that this input is a DNA/RNA sequence
+#' @param start An argument specifying the starting position in the sequence to extract codons
+#'
+#' @return This code defines a function, base_pairs, that takes a DNA or RNA sequence (sequence_length) and divides it into codons
+#' @export
+#'
+#' @examples
+extract_codons <- function(sequence, start = 1){
+  sequence_length <- nchar(sequence)
+  codons <- substring(sequence,
+                      first = seq(from = start, to = sequence_length-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = sequence_length, by = 3))
+  return(codons)
+}
